56a20d909da97db04cc51393f6d186a15b8f5472
Szymon Piecek <onimusht@gmail.com>
2022-01-19 19:58:15 +0000

    Added service for applying discounts

---
+++ src/CheckoutKataKEO.Core/Interfaces/IDiscountService.cs
@@ -0,0 +1,9 @@
+﻿using CheckoutKataKEO.Core.Models;
+
+namespace CheckoutKataKEO.Core.Interfaces
+{
+    public interface IDiscountService
+    {
+        decimal ApplyProductDiscount(ProductLine productLine, Discount discount);
+    }
+}
