435fdab5957649b9351f98ba1d073e5532a55146
Szymon Piecek <onimusht@gmail.com>
2022-01-19 18:46:45 +0000

    Basic basket class

---
+++ src/CheckoutKataKEO.Core/Basket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using CheckoutKataKEO.Core.Models;
+
+namespace CheckoutKataKEO.Core
+{
+    public class Basket
+    {
+        private readonly List<ProductLine> _productLines;
+
+        public Basket()
+        {
+            _productLines = new List<ProductLine>();
+        }
+
+        public int TotalQuantity() => _productLines.Sum(x => x.Quantity);
+
+        public decimal TotalCostWithoutDiscount() => _productLines.Sum(x => x.Quantity * x.Product.Price);
+
+        public void AddProduct(string sku)
+        {
+
+        }
+    }
+}
