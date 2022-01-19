67e4bab438b3573ff22ae708211b1780289dea0e
Szymon Piecek <onimusht@gmail.com>
2022-01-19 19:36:33 +0000

    Added products catalog

---
+++ src/CheckoutKataKEO.Core/Implementation/ProductCatalog.cs
@@ -0,0 +1,42 @@
+﻿using CheckoutKataKEO.Core.Interfaces;
+using CheckoutKataKEO.Core.Models;
+
+namespace CheckoutKataKEO.Core.Implementation
+{
+    public class ProductCatalog : IProductCatalog
+    {
+        private List<Product> _products;
+
+        public ProductCatalog()
+        {
+            InitProductCatalog();
+        }
+
+        public Product GetProduct(string sku)
+        {
+            if (string.IsNullOrWhiteSpace(sku))
+            {
+                throw new ArgumentNullException();
+            }
+
+            var product = _products.FirstOrDefault(x => x.SKU == sku);
+            if (product == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return product;
+        }
+
+        private void InitProductCatalog()
+        {
+            _products = new List<Product>
+            {
+                new("A", 10),
+                new("B", 15),
+                new("C", 40),
+                new("D", 55)
+            };
+        }
+    }
+}
---
+++ src/CheckoutKataKEO.Core/Interfaces/IProductCatalog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheckoutKataKEO.Core.Models;
+
+namespace CheckoutKataKEO.Core.Interfaces
+{
+    public interface IProductCatalog
+    {
+        Product GetProduct(string sku);
+    }
+}
