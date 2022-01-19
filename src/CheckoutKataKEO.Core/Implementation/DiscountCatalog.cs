159d683fa7d5ee76f9653b6c8d88e7faffd29b79
Szymon Piecek <onimusht@gmail.com>
2022-01-19 19:44:39 +0000

    Added discount catalog

---
+++ src/CheckoutKataKEO.Core/Implementation/DiscountCatalog.cs
@@ -0,0 +1,31 @@
+﻿using CheckoutKataKEO.Core.Interfaces;
+using CheckoutKataKEO.Core.Models;
+
+namespace CheckoutKataKEO.Core.Implementation
+{
+    public class DiscountCatalog : IDiscountCatalog
+    {
+        private List<Discount> _discounts;
+
+        public DiscountCatalog()
+        {
+            InitDiscountCatalog();
+        }
+
+        public Discount? GetDiscount(string sku)
+        {
+            if (string.IsNullOrWhiteSpace(sku)) throw new ArgumentNullException();
+
+            var discount = _discounts.FirstOrDefault(x => x.SKU == sku);
+            return discount;
+        }
+
+        private void InitDiscountCatalog()
+        {
+            _discounts = new List<Discount>();
+
+            _discounts.Add(new Discount("B", 40));
+            _discounts.Add(new Discount("D", 25));
+        }
+    }
+}
